8e00587b81e26fedd87183bb89f390efcc441ced
nucym <nucym.dev@gmail.com>
2022-02-06 22:10:18 +0900

    Add app container

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
+import { Container as MuiContainer, Stack, Divider, Box } from "@mui/material";
+
+const Container = () => {
+  return (
+    <MuiContainer maxWidth="sm">
+      <Stack
+        component="nav"
+        direction="row"
+        divider={<Divider orientation="vertical" flexItem />}
+        spacing={2}
+      >
+        <Link to="todo">TODO</Link>
+      </Stack>
+      <Box mt={2}>
+        <Outlet />
+      </Box>
+    </MuiContainer>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Container />}>
+          <Route index element={<>TODO</>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
